8b57e63ce1c485b250a85b0b5e2140d2d1ba1ffa
Damoon <damoon.shah3@gmail.com>
2025-10-27 14:29:11 -0400

    reverting

--- src/prime_rl/orchestrator/config.py
+++ src/prime_rl/orchestrator/config.py
@@ -373,7 +373,8 @@ DataBufferConfigType: TypeAlias = SimpleBufferConfig | DifficultyPoolBufferConfi
 
 
 class AdvantageConfig(BaseConfig):
-    std_norm: Literal["local", "global"] | None = None
+    global_std_norm: bool = False
+    local_std_norm: bool = False
     length_weighted_mean: bool = False
     leave_one_out: bool = False
     neg_clipped: bool = False
